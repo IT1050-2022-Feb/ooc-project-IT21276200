d3119454750b2bac59c0e1324d817c51ce1f9168
IT21261350 <89385432+IT21261350@users.noreply.github.com>
2022-05-18 20:01:21 +0000

    Equipment.cpp is completed

--- Equipment.cpp
+++ Equipment.cpp
@@ -1,22 +1,22 @@
-#include "Equipment.h"
+#include "Equipment.h"  //including Equipment header file
 
-#include <iostream> 
-#include <string>
+#include <iostream>   // including iostream library
+#include <string>   // including string library
 using namespace std;
 
 
-Equipment::Equipment() {
-	equipmentID;
-	equipmentName;
+Equipment::Equipment()  {  // default constructor
+	equipmentID ;
+	equipmentName ;
 	quantity = 0;
 	};
-Equipment::Equipment(string iequipmentID, string iname, int iquantity)
+Equipment::Equipment(string iequipmentID, string iname, int iquantity) // overloaded constructors
 	{
 		equipmentID = iequipmentID;
 		equipmentName = iname;
 		quantity = iquantity;
 	}
-void Equipment::displayDetails()
+void Equipment::displayDetails() // association relation between Equipment and Lab Assistamt
 	{
 		cout << endl;
 		cout << "        Equipment ID    : " << equipmentID << endl;
@@ -24,6 +24,6 @@ void Equipment::displayDetails()
 		cout << "        Quantity        : " << quantity << endl;
 		cout << endl;
 	}
-Equipment::~Equipment() {
+Equipment::~Equipment() { // destructor
 		cout << " --> Deleting Equipment " << endl;
 	}
