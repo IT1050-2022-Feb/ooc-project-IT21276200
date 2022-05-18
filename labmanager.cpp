8f665fbcbe654f2f3c558954acf45f901c918879
IT21359910 <100258921+gavesh625@users.noreply.github.com>
2022-05-18 20:32:54 +0000

    complete

--- labmanager.cpp
+++ labmanager.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+
 LabManager::LabManager(string ufname,string ulname, string unic, string upno,string usf,string upos,int lmid , Lab *l):User(ufname,ulname,unic,upno,usf,upos)
 {
   labManagerID = lmid;
