5fb7cb5fb20acf3d50138ea7ebe32dfa53cf3b28
IT21261350 <89385432+IT21261350@users.noreply.github.com>
2022-05-18 20:07:26 +0000

    LabAssistant.h is completed

--- LabAssistant.h
+++ LabAssistant.h
@@ -1,23 +1,23 @@
 #pragma once
 
-#include "Equipment.h"
-#include "user.h"
-#include <iostream> 
-#include <string>
+#include "Equipment.h"  //including Equipment header file
+#include "user.h"  //including User header file
+#include <iostream>  // including iostream library
+#include <string>  // including string library
 using namespace std;
 
-class LabAssistant:public User
+class LabAssistant:public User //Inheritance relationship detween User and LabAssistant
 {
 private:
 	int labAssistantID;
 	Equipment* eqmt; //an object of Equipment as attribute
 
 public:
-	LabAssistant();
-	LabAssistant(string ufname,string ulname, string unic, string upno,string usf,string upos,int ilabAssistantID, Equipment* e);
+	LabAssistant(); // default constructor
+	LabAssistant(string ufname,string ulname, string unic, string upno,string usf,string upos,int ilabAssistantID, Equipment* e);  // overloaded constructors
 	void addEquipmentDetails(); //association relation between Equipment and Lab Assistamt
-	~LabAssistant();
-	void addEquipment();
-	void editEquipment();
-	void deleteEquipment();
+	void addEquipment(); //addEquipment method
+	void editEquipment(); //editEquipment method
+	void deleteEquipment(); //deleteEquipment method
+  ~LabAssistant();  // destructor
 };
